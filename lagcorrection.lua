60a0bdcc8ceb65904050f4395e30bf3dfb44c439
Anna Salvatòre <nullstephania@gmail.com>
2018-08-14 16:02:42 +0300

    Global changes
    Fixed bugs
    Recoded some functions
    Aded accuracy boost correction
    Added box to select correct fakelag / accuracy boost
    Removed animstate skip
    Replaced maximum ping -> Ping spike threshold

--- lagcorrection.lua
+++ lagcorrection.lua
@@ -5,7 +5,8 @@ local interface = {
 	callback = ui.set_callback,
 	checkbox = ui.new_checkbox,
 	visible = ui.set_visible,
-	slider = ui.new_slider
+	slider = ui.new_slider,
+	multiselect = ui.new_multiselect
 }
 
 local ent = {
@@ -28,26 +29,28 @@ local cl = {
 	realtime = globals.realtime
 }
 
-local flag, flag_hotkey = interface.ref("AA", "Fake lag", "Enabled")
-local slowmo, slowmo_hotkey = interface.ref("AA", "Other", "Slow motion")
-local pingspike, pingspike_hotkey = interface.ref("MISC", "Miscellaneous", "Ping spike")
+-- Some functions
 
-local apr_active = interface.checkbox("MISC", "Miscellaneous", "Anti pingspike reset")
-local apr_maximum = interface.slider("MISC", "Miscellaneous", "Maximum ping", 1, 750, 250, true, "ms")
+local function inArr(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
 
 local function getlatency()
-
 	local prop = ent.get_all("CCSPlayerResource")[1]
-	local g_ServerLatency = ent.get_prop(prop, string.format("%03d", ent.get_local()))
-	local g_RealLatency = math.floor(math.min(1000, cl.latency() * 1000) + 0.5)
+	local latency_client = ent.get_prop(prop, string.format("%03d", ent.get_local()))
+	local latency_server = math.floor(math.min(1000, cl.latency() * 1000) + 0.5)
 
-	g_ServerLatency = (g_ServerLatency > 999 and 999 or g_ServerLatency)
+	latency_client = (latency_client > 999 and 999 or latency_client)
 
-	local g_DeclLatency = g_ServerLatency - g_RealLatency
-	if g_DeclLatency < 1 then g_DeclLatency = 1 end
+	local latency_decl = latency_client - latency_server - 5
+	if latency_decl < 1 then latency_decl = 1 end
 
-	return g_ServerLatency, g_RealLatency, g_DeclLatency
-	
+	return latency_client, latency_server, latency_decl
 end
 
 local function setMath(int, max, declspec)
@@ -78,63 +81,48 @@ local function getColor(number, max)
 	return r, g, b
 end
 
-local function isActive(ping, warn)
-	if warn == 0 then
-		return interface.get(apr_active) and not (interface.get(apr_maximum) > ping)
-	else
-		return interface.get(apr_active) and not interface.get(pingspike_hotkey) and (interface.get(apr_maximum) <= ping)
-	end
-end
-
 local function draw_indicator_circle(c, x, y, r, g, b, a, percentage, outline)
     local outline = outline or true
-    local radius = 9
-    local start_degrees = 0
+    local radius, start_degrees = 9, 0
 
-    -- draw outline
-    if outline then
-        cl.circle_outline(c, x, y, 0, 0, 0, 200, radius, start_degrees, 1.0, 5)
-    end
-    -- draw inner circle
-    cl.circle_outline(c, x, y, r, g, b, 255, radius-1, start_degrees, percentage, 3)
+	if outline then 
+		cl.circle_outline(c, x, y, 0, 0, 0, 200, radius, start_degrees, 1.0, 5)
+	end
+
+    cl.circle_outline(c, x, y, r, g, b, 255, radius-1, start_degrees, percentage, 3) -- Inner Circle
 end
 
-local function visibility(this)
-	interface.visible(apr_maximum, interface.get(this))
-end
+-- Menu
+local flag, flag_hotkey = interface.ref("AA", "Fake lag", "Enabled")
+local slowmo, slowmo_hotkey = interface.ref("AA", "Other", "Slow motion")
+local pingspike, pingspike_hotkey = interface.ref("MISC", "Miscellaneous", "Ping spike")
+local accuracyboost = interface.ref("RAGE", "Other", "Accuracy boost options")
+
+local ms = { "Refine shot", "Extended backtrack" }
+local actions = { "Fakelag trigger", "Accuracy boost correction" }
+
+local apr_active = interface.checkbox("MISC", "Miscellaneous", "Ping spike correction")
+local apr_mselect = interface.multiselect("MISC", "Miscellaneous", "Lag triggers", actions)
+local apr_pingthreshold = interface.slider("MISC", "Miscellaneous", "Ping spike threshold", 1, 750, 250, true, "ms")
+local apr_acthreshold = interface.slider("MISC", "Miscellaneous", "Accuracy boost threshold", 0, 450, 180, true, "ms")
+local apr_acboost = interface.multiselect("MISC", "Miscellaneous", "Accuracy boost flags", ms)
 
 -- Event Functions
 
-local factor, timechange, release_at = 0, 0, nil
-local function on_item_equip(e)
-	local userid, canzoom, item = e.userid, e.canzoom, e.item
-	if userid == nil or factor < 1 then return end
-	if cl.ute(userid) == ent.get_local() then
-		if item == "scar20" or item == "sg556" then
-			cl.exec("-attack2; -lookatweapon; +attack2; +lookatweapon")
-			release_at = cl.realtime() + 0.1
-		else
-			if release_at ~= nil then
-				cl.exec("-attack2; -lookatweapon")
-				release_at = nil
-			end
-		end
-	end
-end
-
+local factor, timechange = 0, 0
 local function on_paint(c)
-	if ent.get_prop(ent.get_local(), "m_iHealth") <= 0 then
+	if not interface.get(apr_active) or ent.get_prop(ent.get_local(), "m_iHealth") <= 0 then
 		return
 	end
 
 	local alpha = 255
-	local g_rLat, g_sLat, g_dLat = getlatency()
+	local latency_client, latency_server, latency_decl = getlatency()
 
-	if interface.get(apr_active) then
-		interface.set(flag, not (interface.get(pingspike_hotkey) and interface.get(apr_maximum) <= g_rLat))
+	if inArr(interface.get(apr_mselect), actions[1]) then
+		interface.set(flag, not (interface.get(pingspike_hotkey) and interface.get(apr_pingthreshold) <= latency_client))
 	end
 
-	local pNum, d = setMath(g_dLat, 250, 100)
+	local pNum, d = setMath(latency_decl, interface.get(apr_pingthreshold), 100)
 	if factor ~= pNum and timechange < cl.realtime() then
 		if factor > pNum then d = -1 else d = 1 end
 		
@@ -143,11 +131,10 @@ local function on_paint(c)
 	end
 
 	local r, g, b = getColor(factor, 100)
-	if not (interface.get(flag) and not isActive(g_rLat, 0)) then	
-		r, g, b = 255, 255, 255
+	if not (interface.get(flag) and interface.get(apr_pingthreshold) > latency_client) then
 
 		local tickcount = (cl.tickcount() % 127.5)
-		if isActive(g_rLat, 1) then
+		if not interface.get(pingspike_hotkey) and (interface.get(apr_pingthreshold) <= latency_client) then
 			if tickcount > 63.75 then
 				alpha = 255 - (tickcount * 4)
 			else
@@ -157,24 +144,52 @@ local function on_paint(c)
 
 	end
 
+	if inArr(interface.get(apr_mselect), actions[1]) and not interface.get(flag) and interface.get(apr_pingthreshold) < latency_client then
+		r, g, b = 53, 110, 254
+	else
+		if inArr(interface.get(apr_mselect), actions[2]) and interface.get(apr_acthreshold) < latency_client then
+			r, g, b = 124, 195, 13
+		end
+	end
+
 	if factor >= 1 then
-
-		if interface.get(pingspike_hotkey) then
-			lr, lg, lb = 255, 255, 255
-		else
-			lr, lg, lb = 255, 0, 0
+		_r, _g, _b = 255, 255, 255
+		if not interface.get(pingspike_hotkey) then
+			_r, _g, _b = 255, 0, 0
 		end
 
-		local y = cl.indicator(c, lr, lg, lb, alpha, "LAG") -- Lag Factor
+		local y = cl.indicator(c, _r, _g, _b, alpha, "LAG") -- Lag Factor
 		draw_indicator_circle(c, 75, (y + 14), r, g, b, alpha, factor / 100)
-
-		if release_at ~= nil and release_at < cl.realtime() then
-			release_at = nil
-			cl.exec("-attack2; -lookatweapon; +lookatweapon; -lookatweapon")
-		end
 	end
 end
 
+local function on_run_cmd(e)
+	if not interface.get(apr_active) or ent.get_prop(ent.get_local(), "m_iHealth") <= 0 then
+		return
+	end
+
+	local choken_n = e.chokedcommands
+	local latency, latency_server, latency_decl = getlatency()
+	
+	if inArr(interface.get(apr_mselect), actions[2]) and interface.get(apr_acthreshold) > 0 then
+	
+		if interface.get(pingspike_hotkey) and interface.get(apr_acthreshold) < latency_decl then
+			interface.set(accuracyboost, interface.get(apr_acboost))
+		else
+			interface.set(accuracyboost, "")
+		end
+
+	end
+end
+
+local function visibility(this)
+	interface.visible(apr_mselect, interface.get(this))
+	interface.visible(apr_pingthreshold, interface.get(this))
+	interface.visible(apr_acthreshold, interface.get(this))
+	interface.visible(apr_acboost, interface.get(this))
+end
+
 interface.callback(apr_active, visibility)
+
 client.set_event_callback("paint", on_paint)
-client.set_event_callback("item_equip", on_item_equip)
+client.set_event_callback("run_command", on_run_cmd)
