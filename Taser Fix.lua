779b0e686d3c18e60e4f0e3ed0afca9f1835ce3b
Anna Salvatòre <nullstephania@gmail.com>
2018-08-30 12:51:56 +0300

    Update (30 Aug, 2018)
    Removed list section
    Fixed some bugs

--- Taser Fix.lua
+++ Taser Fix.lua
@@ -1,43 +1,44 @@
 local interface = {
 	get = ui.get,
 	set = ui.set,
-	ref = ui.reference,
-	callback = ui.set_callback,
-	visible = ui.set_visible
+    ref = ui.reference
 }
 
 local ent = {
 	get_local = entity.get_local_player,
 	get_pweapon = entity.get_player_weapon,
-	get_classname = entity.get_classname
+	get_classname = entity.get_classname,
+	get_prop = entity.get_prop
 }
 
--- Functions
-
-local Options = { "Off", "Delay shot", "Predict" }
-local LC = interface.ref("RAGE", "Other", "Fake lag correction")
-
+local cache = nil
+local ref = interface.ref("RAGE", "Other", "Fake lag correction")
 local isActive = ui.new_checkbox("RAGE", "Other", "Zeus bot correction")
-local DisableLC = ui.new_combobox("RAGE", "Other", "Lag correction list", Options)
 
-local function isVisible(this)
-	interface.visible(DisableLC, interface.get(this))
+local function notAlive(entity)
+	return (entity == nil or ent.get_prop(entity, "m_lifeState") ~= 0)
 end
 
-local function on_run_command(e)
-	if not interface.get(isActive) then 
+local function on_run_command(c)
+    if cache == nil then
+        cache = interface.get(ref)
+    end
+	
+	if not interface.get(isActive) or notAlive(ent.get_local()) then
 		return
 	end
-
+	
 	local weapon = ent.get_pweapon(ent.get_local())
 	local weapon_name = ent.get_classname(weapon)
 
-	if weapon_name ~= "CWeaponTaser" then
-		interface.set(LC, interface.get(DisableLC))
-	else
-		interface.set(LC, "Off")
-	end
+    if weapon_name ~= "CWeaponTaser" then
+        if cache ~= nil then
+            interface.set(ref, cache)
+            cache = nil
+        end
+    else
+		interface.set(ref, "Off")
+    end
 end
 
-interface.callback(isActive, isVisible)
 client.set_event_callback("run_command", on_run_command)
